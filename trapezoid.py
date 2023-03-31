e2313e17f779778b42b45ffa5ab46eb3906d3b04
alamriooooo <129487298+alamriooooo@users.noreply.github.com>
2023-03-31 14:48:08 +0800

    python

---
+++ trapezoid.py
@@ -0,0 +1,5 @@
+longbase = int(input("enter longbase: "))
+shortbase = int(input("enter shortbade: "))
+height = int(input("enter height: "))
+area = ((longbase + shortbase) /2)*height
+print("area of trapeziod:", int(area))
---
+++ userpass.py
@@ -0,0 +1,24 @@
+attempts=True
+
+while attempts==True:
+
+    username=input("Enter username: \n")
+
+    password=input("Enter password:\n")
+
+    if username=="andrean"and password=="delirious":
+
+        print("sakto ka gid ya")
+
+        attempts=False
+
+       
+
+    else:
+
+        print("sala user kag pass mo ya")
+
+        
+
+            
+
